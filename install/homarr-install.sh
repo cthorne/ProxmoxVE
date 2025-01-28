e362cce35ccf9cd2bccd3f575d753ded7b591383
cthorne <christhorn3@gmail.com>
2025-01-28 20:44:12 +1000

    Update homarr-install.sh

--- install/homarr-install.sh
+++ install/homarr-install.sh
@@ -56,11 +56,11 @@ cd /opt/homarr
 
 
 msg_info "Installing Homarr (pnpm)"
+mkdir build
+cp ./node_modules/better-sqlite3/build/Release/better_sqlite3.node ./build/better_sqlite3.node
 $STD pnpm install
 msg_info "Building Homarr"
 $STD pnpm build
-mkdir build
-cp ./node_modules/better-sqlite3/build/Release/better_sqlite3.node ./build/better_sqlite3.node
 msg_info "DB migration Homarr"
 $STD pnpm run db:migration:sqlite:run
 echo "${RELEASE}" >"/opt/${APPLICATION}_version.txt"
