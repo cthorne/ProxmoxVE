afa1ce9c8f165ba347e680a085bd0d455c640dcc
cthorne <christhorn3@gmail.com>
2025-01-28 19:53:23 +1000

    Update homarr-install.sh

--- install/homarr-install.sh
+++ install/homarr-install.sh
@@ -33,7 +33,7 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dea
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
 msg_ok "Set up Node.js Repository"
 
-msg_info "Installing Node.js/Corepack/pnpmn"
+msg_info "Installing Node.js/Corepack/pnpm"
 $STD apt-get update
 $STD apt-get install -y nodejs
 $STD corepack install --global pnpm@9.15.4
